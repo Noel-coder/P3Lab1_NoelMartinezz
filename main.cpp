d30cb59d6c92bb7ac680852802a4e41682b87030
Noel-coder <noelgg_23@outlook.es>
2021-01-22 15:23:45 -0600

    Push Final

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -7,22 +9,48 @@ using std::endl;
 
 int main(int argc, char** argv) {
 	int opcion=1;
-	while(opcion != 3){
+	while(opcion != 3) {
 		cout << "Menu" << endl
-		<< "1. Sumatoria" << endl
-		<< "2. Funcion Sigmoide" << endl
-		<< "Salir"<< endl;
+		     << "1. Sumatoria" << endl
+		     << "2. Funcion Sigmoide" << endl
+		     << "3. Salir"<< endl;
 		cout << "Ingrese una opcion: " << endl;
 		cin>>opcion;
-		switch(opcion){
-			case 1:{
+		switch(opcion) {
+			case 1: {
 				//sumatoria
-				
+				cout << "Ingrese n: ";
+				int numero=0;
+				cin >> numero;
+				while(numero < 0) {
+					cout << "¡ Porfavor ingrese un numero positivo !";
+					cin>> numero;
+				}
+				int acumulador = 0;
+				for(int i = 0; i <= numero; i++) {
+					acumulador+= i*pow(i+1,2);
+				}
+				cout << "La respuesta es: " << acumulador << endl;
 				break;
 			}
-			case 2:{
+			case 2: {
 				//funcion Sigmoide
-				
+				double resultado;
+				cout << "Ingrese x: ";
+				int numero;
+				cin>> numero;
+
+				resultado += 1/(1+exp(numero));
+				cout << "adad" << resultado << endl;
+
+				cout << std::fixed;
+				cout << std::setprecision(6);
+				cout << "El resultado es: " << resultado << endl;
+
+				break;
+			}
+			case 3: {
+				cout << "Gracias por usar el programa :)" << endl;
 				break;
 			}
 		}
